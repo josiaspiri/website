0e69af09a2f0cd94cd5eb0997305bbbd604a82f8
Josias Piri <sojias@gmail.com>
2021-11-28 23:27:43 -0500

    improvements

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,14 +4,10 @@ import GitCards from './GitCards';
 
 export default function App() {
   return (
-    <div className='content flex flex-row-wrap margin-auto children-bottom-margin'>
-      <div className='flex flex-fill-width flex-horizontal-center'>
-        <h1 className='center-text'>Josias Piri</h1>
-      </div>
-      <div className='flex flex-fill-width flex-horizontal-center'>
-        <p className='center-text seesaw italic'>Howdy, neighbor!</p>
-      </div>
-      <div className='flex flex-fill-width flex-horizontal-center'>
+    <div className='content'>
+        <h1>Josias Piri</h1>
+        <p className='seesaw italic'>Howdy, neighbor!</p>
+      <div className='flex'>
         <a href="https://www.linkedin.com/in/josias-piri-0a4a71aa" className='circle icon-container'>
           <FontAwesomeIcon icon={faLinkedin} />
         </a>
